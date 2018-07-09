d5401ea57a02ebed7c73b7abce7ae96c4a415dc2
chenran01 <chenran01@360.net>
2018-07-09 18:01:46 +0800

    v1.5

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
-#pip2 install simplejson
-#pip2 install psutil
-#pip2 install httplib
-#pip2 install mimetypes
-
+pip2 install simplejson
+pip2 install psutil
+pip2 install httplib
+pip2 install mimetypes
 path=`pwd`
 command1="alias emg='python "
 command2="alias whois='python "
 command3="alias vt='python "
 files1="/emergency.py'"
-files2="/mywhois.py"
-files3="/virustotal.py"
-var1=$command1$path
-var2=$var1$file1
-#var2={$command2}{$path}{$file2}
-#var3={$command3}{$path}{$file3}
-echo $var2
+files2="/mywhois.py'"
+files3="/virustotal.py'"
+var1=$command1$path$files1
+var2=$command2$path$files2
+var3=$command3$path$files3
+echo $var1 >> /root/.bash_profile
+echo $var2 >> /root/.bash_profile
+echo $var3 >> /root/.bash_profile
+source /root/.bash_profile
